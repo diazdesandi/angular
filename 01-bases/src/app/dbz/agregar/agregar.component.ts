a86421f5512db60b1d26b61fec846bd3a4a31572
René <diaz.de.sandi.7@gmail.com>
2023-03-19 15:01:00 -0700

    input/output, components

---
+++ 01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+  @Input('nuevo') nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
+
+  // Sirve para emitir eventos.
+  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+    this.onNuevoPersonaje.emit(this.nuevo);
+    // console.log(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0,
+    };
+  }
+}
