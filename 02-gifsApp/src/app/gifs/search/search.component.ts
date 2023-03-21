03bc1b910f33f94ab10af8137563ad50f9a250c5
René <diaz.de.sandi.7@gmail.com>
2023-03-21 16:27:22 -0700

    gifs

---
+++ 02-gifsApp/src/app/gifs/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styles: [],
+})
+export class SearchComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) {}
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    // console.log(valor);
+    this.gifsService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = '';
+  }
+}
