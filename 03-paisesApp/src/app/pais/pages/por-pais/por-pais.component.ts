9ced78da754583d31e26aa7f72e54adced6854ae
René <diaz.de.sandi.7@gmail.com>
2023-03-23 18:49:37 -0700

    adding debounce to search input

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -9,19 +10,27 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) {}
 
-  buscar() {
-    console.log(this.termino);
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
 
-    this.paisService.buscarPais(this.termino).subscribe(
-      (resp) => {
-        console.log(resp);
+    this.paisService.buscarPais(this.termino).subscribe({
+      next: (paises) => {
+        console.log(paises);
+        this.paises = paises;
       },
-      (err) => {
+      error: (err) => {
         this.hayError = true;
-      }
-    );
+        this.paises = [];
+      },
+    });
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
   }
 }
