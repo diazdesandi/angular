063e922873ab6d6853aec54ec1e26e2f41476ecc
René <diaz.de.sandi.7@gmail.com>
2023-03-29 21:11:35 -0700

    json db

---
+++ 05-heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { Component, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'heroes',
+    loadChildren: () =>
+      import('./heroes/heroes.module').then((m) => m.HeroesModule),
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent,
+  },
+  {
+    path: '**',
+    // component: ErrorPageComponent,
+    redirectTo: '404',
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
