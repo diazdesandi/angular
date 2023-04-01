9c1bcfd28c5aa054ab7a158f66c99bbc473ffdd9
René <diaz.de.sandi.7@gmail.com>
2023-03-31 22:03:53 -0700

    img pipe

--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -8,6 +8,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../material/material.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { MaterialModule } from '../material/material.module';
     HeroeComponent,
     ListadoComponent,
     HomeComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe,
   ],
   imports: [
     CommonModule,
--- 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styles: [
-  ]
+    `
+      mat-card {
+        margin-top: 25px;
+      }
+    `,
+  ],
 })
 export class ListadoComponent implements OnInit {
+  heroes: Heroe[] = [];
 
-  constructor() { }
+  constructor(private heroeService: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroeService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
-
 }
