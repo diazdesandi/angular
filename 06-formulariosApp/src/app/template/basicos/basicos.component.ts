f4441e42dfd2d12b4a9a3dfd0440685ad5918985
René <diaz.de.sandi.7@gmail.com>
2023-04-06 00:50:05 -0700

    custom directive

--- 06-formulariosApp/src/app/template/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -1,11 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
 })
 export class BasicosComponent implements OnInit {
+  // Para ver el elemento de una referencia local
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls['producto']?.invalid &&
+      this.miFormulario?.controls['producto']?.touched
+    );
+  }
+  precioValido(): boolean {
+    return (
+      this.miFormulario?.controls['precio']?.value < 0 &&
+      this.miFormulario?.controls['precio']?.touched
+    );
+  }
+
+  guardar() {
+    console.log(this.miFormulario);
+  }
 }
